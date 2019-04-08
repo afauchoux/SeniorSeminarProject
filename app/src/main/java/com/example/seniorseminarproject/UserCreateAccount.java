b8e34ec8242cccc103d94b5b7cf0fac1a7656c6d
afauchoux <33325666+afauchoux@users.noreply.github.com>
2019-04-07 22:32:01 -0500

    User Authorization completed for login

--- app/src/main/java/com/example/seniorseminarproject/UserCreateAccount.java
+++ app/src/main/java/com/example/seniorseminarproject/UserCreateAccount.java
@@ -1,43 +1,47 @@
 package com.example.seniorseminarproject;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class UserCreateAccount extends AppCompatActivity {
 
-    public String userId;
-    public EditText userCreateFirstNameET;
-    public EditText userCreateLastNameET;
-    public EditText userCreateUsernameET;
     public EditText userCreateEmailET;
     public EditText userCreatePasswordET;
-    public EditText userCreatePasswordET2;
     public Button userCreateAccountButton;
     public DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    public ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_create_account);
 
-        this.userCreateFirstNameET = (EditText) this.findViewById(R.id.userCreateFirstNameET);
-        this.userCreateLastNameET = (EditText) this.findViewById(R.id.userCreateLastNameET);
-        this.userCreateUsernameET = (EditText) this.findViewById(R.id.userCreateUsernameET);
         this.userCreateEmailET = (EditText) this.findViewById(R.id.userCreateEmailET);
         this.userCreatePasswordET = (EditText) this.findViewById(R.id.userCreatePasswordET);
-        this.userCreatePasswordET2 = (EditText) this.findViewById(R.id.userRetypePasswordET);
         this.userCreateAccountButton = (Button) this.findViewById(R.id.userCreateAccountButton);
-        mDatabase = FirebaseDatabase.getInstance().getReference("users");
+        this.progressBar = (ProgressBar) this.findViewById(R.id.progressBar);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void userCreateAccountButtonPressed(View v){
@@ -46,28 +50,53 @@ public class UserCreateAccount extends AppCompatActivity {
     }
 
     private void createNewUser(){
-        String username = this.userCreateUsernameET.getText().toString().trim();
-        String password = this.userCreatePasswordET.getText().toString().trim();
-        String firstName = this.userCreateFirstNameET.getText().toString().trim();
-        String lastName = this.userCreateLastNameET.getText().toString().trim();
-        String email = this.userCreateEmailET.getText().toString().trim();
-        //Boolean isAdmin = false;
+        String email = userCreateEmailET.getText().toString().trim();
+        String password = userCreatePasswordET.getText().toString().trim();
 
-        if((!TextUtils.isEmpty(username)) && (!TextUtils.isEmpty(password))){
-
-            String userId = mDatabase.push().getKey();
-
-            User user = new User(userId, username, password, firstName, lastName, email, false);
-
-            mDatabase.child(userId).setValue(user);
-
-            Toast.makeText(this, "User Created Successfully", Toast.LENGTH_LONG).show();
-
-            Intent i = new Intent(this, UserLogin.class);
-            this.startActivity(i);
+        if(email.isEmpty()){
+            userCreateEmailET.setError("Email is required");
+            userCreateEmailET.requestFocus();
+            return;
         }
-        else{
-            Toast.makeText(this, "Please enter all information", Toast.LENGTH_LONG).show();
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            userCreateEmailET.setError("Please enter a valid email");
+            userCreateEmailET.requestFocus();
+            return;
         }
+
+        if(password.isEmpty()){
+            userCreatePasswordET.setError("Password is required");
+            userCreatePasswordET.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            userCreatePasswordET.setError("Minimum length of password is 6 characters");
+            userCreatePasswordET.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "User created successfully", Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(getApplicationContext(), UserLogin.class);
+                    getApplicationContext().startActivity(i);
+                }
+                else{
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                        Toast.makeText(getApplicationContext(), "Email is already in use", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/seniorseminarproject/UserLogin.java
+++ app/src/main/java/com/example/seniorseminarproject/UserLogin.java
@@ -3,14 +3,20 @@ package com.example.seniorseminarproject;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,8 +29,13 @@ public class UserLogin extends AppCompatActivity {
     public TextView userCreateAccountTextPressed;
     public TextView userForgotPasswordTextPressed;
     public TextView adminLoginTextPressed;
-    public EditText userUsernameET;
+    public EditText userEmailET;
+    public EditText userPasswordET;
+    public ProgressBar progressBarLogin;
+
     public DatabaseReference mDatabase;
+    public FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -32,8 +43,12 @@ public class UserLogin extends AppCompatActivity {
         setContentView(R.layout.activity_user_login);
 
         this.userLoginButton = (Button)this.findViewById(R.id.userLoginButton);
-        this.userUsernameET = (EditText)this.findViewById(R.id.userUsernameET);
+        this.userEmailET = (EditText)this.findViewById(R.id.userEmailET);
+        this.userPasswordET = (EditText)this.findViewById(R.id.userPasswordET);
+        this.progressBarLogin = (ProgressBar)this.findViewById(R.id.progressBarLogin);
+
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        this.mAuth = FirebaseAuth.getInstance();
     }
 
     public void userCreateAccountTextPressed(View v){
@@ -42,11 +57,7 @@ public class UserLogin extends AppCompatActivity {
     }
 
     public void userLoginButtonPressed(View v){
-        String username = this.userUsernameET.getText().toString().trim();
-
-            Toast.makeText(this, "Successful Login", Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(this, UserMainActivity.class);
-            this.startActivity(i);
+        userLogin();
     }
 
     public void userForgotPasswordTextPressed(View v){
@@ -57,6 +68,53 @@ public class UserLogin extends AppCompatActivity {
     public void adminLoginTextPressed(View v){
         Intent i = new Intent(this, AdminLogin.class);
         this.startActivity(i);
+    }
+
+    private void userLogin(){
+        String email = userEmailET.getText().toString().trim();
+        String password = userPasswordET.getText().toString().trim();
+
+        if(email.isEmpty()){
+            userEmailET.setError("Email is required");
+            userEmailET.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            userEmailET.setError("Please enter a valid email");
+            userEmailET.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            userPasswordET.setError("Password is required");
+            userPasswordET.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            userPasswordET.setError("Minimum length of password is 6 characters");
+            userPasswordET.requestFocus();
+            return;
+        }
+
+        progressBarLogin.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBarLogin.setVisibility(View.GONE);
+                if(task.isSuccessful()){
+                    finish();
+                    Intent i = new Intent(getApplicationContext(), UserMainActivity.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
 }
