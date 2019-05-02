20df71a22ff61fae50aaa84f8bb40c84b25d5292
afauchoux <33325666+afauchoux@users.noreply.github.com>
2019-05-02 15:22:59 -0500

    Completed ability to get back events from database

--- app/src/main/java/com/example/seniorseminarproject/RecyclerViewConfig.java
+++ app/src/main/java/com/example/seniorseminarproject/RecyclerViewConfig.java
@@ -1,13 +1,25 @@
 package com.example.seniorseminarproject;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class RecyclerViewConfig {
     private Context mContext;
+    private EventsAdapter mEventsAdapter;
+
+    public void setConfig(RecyclerView recyclerView, Context context, List<Event> events, List<String> keys){
+        mContext = context;
+        mEventsAdapter = new EventsAdapter(events, keys);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(mEventsAdapter);
+    }
 
     class EventItemView extends RecyclerView.ViewHolder{
         private TextView mEventName;
@@ -29,8 +41,38 @@ public class RecyclerViewConfig {
         }
 
         public void bind(Event event, String key){
-            //mEventName.getText(event.getEventName());
+            mEventName.setText(event.getEventName());
+            mEventDate.setText(event.getEventDate());
+            mEventTime.setText(event.getEventTime());
+            mEventDescription.setText(event.getEventDescription());
+            mEventPoints.setText(event.getEventPoints());
+            this.key = key;
+        }
+    }
 
+    class EventsAdapter extends RecyclerView.Adapter<EventItemView>{
+        private List<Event> mEventList;
+        private List<String> mKeys;
+
+        public EventsAdapter(List<Event> mEventList, List<String> mKeys) {
+            this.mEventList = mEventList;
+            this.mKeys = mKeys;
+        }
+
+        @NonNull
+        @Override
+        public EventItemView onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return new EventItemView(viewGroup);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull EventItemView eventItemView, int i) {
+            eventItemView.bind(mEventList.get(i), mKeys.get(i));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mEventList.size();
         }
     }
 }
--- app/src/main/java/com/example/seniorseminarproject/UserEvents.java
+++ app/src/main/java/com/example/seniorseminarproject/UserEvents.java
@@ -3,15 +3,41 @@ package com.example.seniorseminarproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import java.util.List;
+
 public class UserEvents extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_events);
+        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerviewEvents);
+        new EventDatabaseHelper().readEvents(new EventDatabaseHelper.DataStatus() {
+            @Override
+            public void DataLoaded(List<Event> events, List<String> keys) {
+                new RecyclerViewConfig().setConfig(mRecyclerView, UserEvents.this, events, keys);
+            }
 
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdated() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
     }
 
     public void eventsBackButtonPressed(View v){
